5f8e335784414a201d53606c37455e970f8798de
aastarita <anthony@astarita.me>
2015-01-24 11:02:15 -0500

    Final Version
    This is the final version with comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix(x) creates a special "matrix" object
+## that can cache its inverse. it returns a list
+## that contains a function to:
+## - set the value of the "matrix"
+## - get the value of the "matrix"
+## - set the value of the "matrix"
+## - get the value of the "matrix"
 
-## Write a short comment describing this function
+## Example:   'x'      [,1] [,2]
+##               [1,]    4    6
+##               [2,]    5    7
+## creates a special "matrix" object
+
+## Precondition: 'x' is a "square" matrix that is "invertible."
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -12,23 +22,35 @@ makeCacheMatrix <- function(x = matrix()) {
         get <- function() x
         setinverse <- function(inverse) m <<- inverse
         getinverse <- function() m
-        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+        list(set = set, get = get, setinverse = setinverse, 
+             getinverse = getinverse)
 }
 
+## This function computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above. If the inverse has already 
+## been calculated (and the matrix has not changed), then  
+## cacheSolve should retrieve the inverse from the cache. The
+## function returns a matrix that is the inverse of 'x'
 
-## Write a short comment describing this function
+## Example:   'x'       [,1] [,2]
+##              [1,]    4    6
+##              [2,]    5    7
+## cacheSolve(makeCacheMatrix(x) returns inverse of 'x':
+##                      [,1] [,2]
+##              [1,]    -3.5    3
+##              [2,]    2.5   -2
 
-cacheSolve <- function(x, ...) {
-## Return a matrix that is the inverse of 'x'
-        m <- x$getinverse()
+## Precondition: 'x_list' is the special "matrix" object that is 
+## returned from executing the makeCacheMatrix function.
+
+cacheSolve <- function(x_list, ...) {
+        m <- x_list$getinverse()
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
-        data <- x$get()
+        data <- x_list$get()
         m <- solve(data)
-        x$setinverse(m)
+        x_list$setinverse(m)
         m
-        
-
 }
